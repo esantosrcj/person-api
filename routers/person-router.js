ced6dc4f57a5e20fb0b05f8d005b4b57c7b34969
Eddie Santos <esantosrcj@gmail.com>
2023-04-18 22:47:58 -0500

    Add CRUD operations for person

---
+++ routers/person-router.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { personRepository } from '../om/person.js';
+
+export const router = Router();
+
+// CREATE
+router.put('/', async (req, res) => {
+    const person = await personRepository.createAndSave(req.body);
+    res.send(person);
+});
+
+// READ
+router.get('/:id', async (req, res) => {
+    const person = await personRepository.fetch(req.params.id);
+    res.send(person);
+});
+
+// UPDATE
+router.post('/:id', async (req, res) => {
+    const person = await personRepository.fetch(req.params.id);
+
+    person.firstName = req.body.firstName ?? null;
+    person.lastName = req.body.lastName ?? null;
+    person.age = req.body.age ?? null;
+    person.verified = req.body.verified ?? null;
+    person.location = req.body.location ?? null;
+    person.locationUpdated = req.body.locationUpdated ?? null;
+    person.skills = req.body.skills ?? null;
+    person.personalStatement = req.body.personalStatement ?? null;
+
+    await personRepository.save(person);
+
+    res.send(person);
+});
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+    await personRepository.remove(req.params.id);
+    res.send({ entityId: req.params.id });
+});
