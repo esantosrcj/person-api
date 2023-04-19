a93aaf5266968b14123f2043edda783e593c4c4d
Eddie Santos <esantosrcj@gmail.com>
2023-04-18 23:53:45 -0500

    Store location history with Streams

--- routers/location-router.js
+++ routers/location-router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { personRepository } from '../om/person.js';
+import { connection } from '../om/client.js';
 
 export const router = Router();
 
@@ -14,6 +15,10 @@ router.patch('/:id/location/:lng,:lat', async (req, res) => {
     person.location = { longitude, latitude };
     person.locationUpdated = locationUpdated;
     await personRepository.save(person);
+
+    // NEEDS WORK: Storing location history with Streams
+    let keyName = `${person.keyName}:locationHistory`;
+    await connection.XADD(keyName, '*', person.location);
 
     res.send({ id, locationUpdated, location: { longitude, latitude } });
 });
